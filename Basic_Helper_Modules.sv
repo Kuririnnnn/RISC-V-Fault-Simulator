f2b4ca0258f8bd7f9ab60ac3262e805ffbe52243
Shashank Rao Pejaver <shashankraopejaver@gmail.com>
2025-08-09 17:45:33 +0530

    Update Basic_Helper_Modules.sv
    Contains basic combinational helper modules for the single-cycle RISC-V processor. Includes a 32-bit Adder for program counter updates and a 2-to-1 Multiplexer for data path selection.

--- Basic_Helper_Modules.sv
+++ Basic_Helper_Modules.sv
@@ -1,13 +1,15 @@
-// -------------- Basic Helper Modules --------------
+// -------------------- Basic Helper Modules --------------------
 
 `timescale 1ns/1ps
 
+// ---------------------------- Adder ----------------------------
 module PC_Adder (a,b,c);
     input [31:0] a, b;
     output [31:0] c;
     assign c = a + b;
 endmodule
 
+// ------------------------- Multiplexer -------------------------
 module Mux (a,b,s,c);
   	input [31:0] a, b;
     input s;
