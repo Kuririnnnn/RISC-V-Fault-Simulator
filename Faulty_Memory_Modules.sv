7d6c4e40415d9a83bcb3b11162ca918aada4d76b
Shashank Rao Pejaver <shashankraopejaver@gmail.com>
2026-07-15 12:09:18 +0530

    Refactor faulty memory modules for parameterization

--- Faulty_Memory_Modules.sv
+++ Faulty_Memory_Modules.sv
@@ -1,21 +1,20 @@
 // ------------------- Faulty Processor Memory Modules -------------------
-
 `timescale 1ns/1ps
 
 // ------------------------- Data Memory (Faulty) -------------------------
-module Data_Memory_faulty(clk, rst, WE, WD, A, RD);
-    input clk, rst, WE;
+module Data_Memory_faulty #(parameter MEM_DEPTH = 1024) (clk, rst_n, WE, WD, A, RD);
+    input clk, rst_n, WE;
     input [31:0] A, WD;
     output [31:0] RD;
 
-    reg [31:0] mem [1023:0];
+    reg [31:0] mem [MEM_DEPTH-1:0];
 
     always @(posedge clk) begin
         if (WE)
             mem[A] <= WD;
     end
 
-    assign RD = (~rst) ? 32'd0 : mem[A];
+    assign RD = (~rst_n) ? 32'd0 : mem[A];
 
     initial begin
         mem[28] = 32'h00000020;
@@ -23,20 +22,20 @@ module Data_Memory_faulty(clk, rst, WE, WD, A, RD);
 endmodule
 
 // ---------------------- Instruction Memory (Faulty) ----------------------
-module Instruction_Memory_faulty(rst, A, RD);
-    input rst;
+module Instruction_Memory_faulty #(parameter MEM_DEPTH = 1024) (rst_n, A, RD);
+    input rst_n;
     input [31:0] A;
     output [31:0] RD;
 
-    reg [31:0] mem [1023:0];
+    reg [31:0] mem [MEM_DEPTH-1:0];
     reg [31:0] RD_out;
 
     always @(A) begin
         RD_out = mem[A[31:2]];
-        RD_out[0] = 1'b0;  // Simulate bit-level stuck-at-0 fault
+        RD_out[0] = 1'b0; // Simulate bit-level stuck-at-0 fault
     end
 
-    assign RD = (~rst) ? 32'd0 : RD_out;
+    assign RD = (~rst_n) ? 32'd0 : RD_out;
 
     initial begin
         mem[0] = 32'h0062E233;
@@ -49,14 +48,14 @@ module Instruction_Memory_faulty(rst, A, RD);
 endmodule
 
 // ------------------------- Program Counter (Faulty) -------------------------
-module PC_Module_faulty(clk, rst, PC, PC_Next);
-    input clk, rst;
+module PC_Module_faulty(clk, rst_n, PC, PC_Next);
+    input clk, rst_n;
     input [31:0] PC_Next;
     output [31:0] PC;
     reg [31:0] PC;
 
     always @(posedge clk) begin
-        if (~rst)
+        if (~rst_n)
             PC <= {32{1'b0}};
         else
             PC <= PC_Next;
@@ -64,8 +63,8 @@ module PC_Module_faulty(clk, rst, PC, PC_Next);
 endmodule
 
 // ------------------------- Register File (Faulty) -------------------------
-module Register_File_faulty(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
-    input clk,rst,WE3;
+module Register_File_faulty(clk,rst_n,WE3,WD3,A1,A2,A3,RD1,RD2);
+    input clk,rst_n,WE3;
     input [4:0]A1,A2,A3;
     input [31:0]WD3;
     output [31:0]RD1,RD2;
@@ -77,12 +76,12 @@ module Register_File_faulty(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
             reg_file[A3] <= WD3;
     end
 
-    assign RD1 = (~rst) ? 32'd0 : reg_file[A1];
-    assign RD2 = (~rst) ? 32'd0 : reg_file[A2];
+    assign RD1 = (~rst_n) ? 32'd0 : reg_file[A1];
+    assign RD2 = (~rst_n) ? 32'd0 : reg_file[A2];
 
     initial begin
-        reg_file[5] = 32'h5;
-        reg_file[6] = 32'h4;
+        reg_file[5]  = 32'h5;
+        reg_file[6]  = 32'h4;
         reg_file[11] = 32'h3;
         reg_file[12] = 32'h2;
         reg_file[18] = 32'h5;
@@ -99,7 +98,6 @@ module Sign_Extend_faulty(In,Imm_Ext,ImmSrc);
     input [31:0]In;
     input ImmSrc;
     output [31:0]Imm_Ext;
-
     assign Imm_Ext = (ImmSrc == 1'b1) ? {{20{In[31]}},In[31:25],In[11:7]} :
-                                        {{20{In[31]}},In[31:20]};
+                      {{20{In[31]}},In[31:20]};
 endmodule
